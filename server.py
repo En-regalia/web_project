18347d39e9288aa9d598f73e124b5deddbffafa7
En-regalia <mike.ranner1@gmail.com>
2024-10-07 20:19:21 +0100

    Updated server.py. Introduced inital do_get function to handel and return user_id queries

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse, parse_qs
 import sqlite3
 import os
 
-#note: will need to impliment password hashing at a later date.
+#note: will need to impliment password hashing at a later date. (bcrypt)
 def initiliseDatabase(db_name):
     # check for database presence in the directory.
     if not os.path.exists(db_name):
@@ -20,8 +20,47 @@ def initiliseDatabase(db_name):
             )
         ''')
         print("Database "&db_name&" initated and table created sucessfuly")
+        conn.close()
     else:
         print("Database check completed"& db_name&" is present. Exiting function")
 
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_get(self):
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
+
+        initiliseDatabase("users.db")
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        if "email" in query_params:
+            email = query_params['email'][0]
+
+            cursor.execute("SELECT user_id FROM users WHERE email = ?", (email,))
+            result = cursor.fetchone()
+
+            user_id = result[0]
+
+            response = f"User ID for the email {email} is: {user_id}"
+
+            if response:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(response.encode())
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b"User not found for the provided email")
+            
+        else:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Bad request: Missing email")
+        
 
 
