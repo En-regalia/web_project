bfe3c03de8fd259d5eedf9e79d1aabae4fe9ee0e
En-regalia <mike.ranner1@gmail.com>
2024-10-07 19:40:17 +0100

    Updated Server file. Introduced initalise Database function

--- server.py
+++ server.py
@@ -1,5 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
+import sqlite3
+import os
+
+#note: will need to impliment password hashing at a later date.
+def initiliseDatabase(db_name):
+    # check for database presence in the directory.
+    if not os.path.exists(db_name):
+        # if not found. Initally creates DB and creates table. 
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            CREAT TABLE IF NOT EXISTS users(
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE
+                password TEXT NOT NULL 
+            )
+        ''')
+        print("Database "&db_name&" initated and table created sucessfuly")
+    else:
+        print("Database check completed"& db_name&" is present. Exiting function")
 
 
 
