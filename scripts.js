be63d1b7961775ae73a06914fdd35b218daa8213
En-regalia <mike.ranner1@gmail.com>
2024-10-13 20:54:12 +0100

    Updated Scrips.js to contan inital HTTP get reuest Fetch function

--- scripts.js
+++ scripts.js
@@ -2,4 +2,19 @@ const server_url =
 
 document.getElementById("get-home").addEventListener("click", function(event) {
     event.preventDefault();
+
+    fetch("HTTP://ec2-13-43-90-147.eu-west-2.compute.amazonaws.com")
+        .then(response =>{
+            if(!response.ok) {
+                throw new Error ("Request was not ok"& response.statusText)
+            }
+            return response.text();
+        })
+        .then (html => {
+            document.getElementById("card-container").innerHTML = html;
+            console.log("Homepage requested and recived")
+        })
+        .catch(error => {
+            console.error("There was a problem witht he get request")
+        })
 })
