c2267f5c81b5cc9f9b5843286a4836da5ba1e827
En-regalia <mike.ranner1@gmail.com>
2024-10-14 20:18:09 +0100

    Added Consol.logs for testing

--- scripts.js
+++ scripts.js
@@ -4,10 +4,12 @@ document.getElementById("get-home").addEventListener("click", function(event) {
     event.preventDefault();
 
     fetch("HTTP://ec2-13-43-90-147.eu-west-2.compute.amazonaws.com")
+        console.log("Sending GET request")
         .then(response =>{
             if(!response.ok) {
                 throw new Error ("Request was not ok"& response.statusText)
             }
+            console.log("Responce recived")
             return response.text();
         })
         .then (html => {
