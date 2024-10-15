3c542c3ada2f93e5f707f4da651f31580bb3df93
En-regalia <mike.ranner1@gmail.com>
2024-10-15 19:56:24 +0100

    Added event listener for 'DOMContentLoaded' to ensure scripts.js functionality. Moved Script.js link in index.html to head from body

--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('get-home').addEventListener("click", function(event) {
+        event.preventDefault();
 
-document.getElementById('get-home').addEventListener("click", function(event) {
-    event.preventDefault();
+        console.log("Sending GET request");
 
-    console.log("Sending GET request");
-
-    fetch("http://localhost:8000")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Request was not ok: " + response.status);
-            }
-            console.log("Response received");
-            return response.text();
-        })
-        .then(html => {
-            console.log("Card Container Found:", cardContainer);
-            console.log('HTML found:', html)
-            document.querySelector("#cardContainer").innerHTML = html;
-            console.log("Homepage requested and received");
-        })
-        .catch(error => {
-            console.error("There was a problem with the GET request:", error);
-        });
+        fetch("http://localhost:8000")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Request was not ok: " + response.status);
+                }
+                console.log("Response received");
+                return response.text();
+            })
+            .then(html => {
+                console.log("Card Container Found:", cardContainer);
+                console.log('HTML found:', html)
+                document.querySelector("#cardContainer").innerHTML = html;
+                console.log("Homepage requested and received");
+            })
+            .catch(error => {
+                console.error("There was a problem with the GET request:", error);
+            });
+    });
 });
